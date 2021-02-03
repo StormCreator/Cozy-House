06e615369e48158d1d3517542e8988b3774bc190
MarginToppson <insetml@gmail.com>
2021-02-03 19:51:12 +0200

    added slider friends section

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,10 @@
-// import hamburger from './modules/hamburger';
-
-// $(document).ready(function () {
-
-//     hamburger();
-
-// });
-
-
+$(document).ready(function(){
+    $('.friends__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        arrows: true,
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/prev-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next-arrow.png"></button>',
+    });
+})(jQuery);
